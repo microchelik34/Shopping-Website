feb4672a683fc3b2897064c8c0201ef264525116
microchelik34 <demin.artem2003@gmail.com>
2024-08-12 20:31:55 +0300

    add Hero and Sponsors bars

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: ['./index.html'],
   theme: {
+    borderRadius: {
+      'megaLarge':'59px',
+      'lg':'8px',
+    },
+
     extend: {
       fontFamily:{
         Poppins:['Poppins', 'sans-serif'],
@@ -10,10 +15,12 @@ module.exports = {
         'Yellow': '#E6C744',
         'Blue': '#C2C8DA',
         'TextColor': '#242323',
+        'bgHero': '#f4f6f5',
       },
       letterSpacing: {
         wide: '.01em',
         narrow: '-.03em',
+        narrowest: '-.05em',
       },
     },
   },
