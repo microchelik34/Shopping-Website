77c85dba1e82f26f432c029be54dfbc6d75cb18c
microchelik34 <demin.artem2003@gmail.com>
2024-08-20 15:43:04 +0300

    add sections
    New Arrivals, PayDay, Favourite, Download, Mail

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,11 +13,14 @@ module.exports = {
       },
       colors: {
         'Yellow': '#E6C744',
+        'YellowBG': '#e5c643',
         'Blue': '#C2C8DA',
         'TextColor': '#242323',
+        'TextGray': '#7f7f7f',
         'bgHero': '#f4f6f5',
       },
       letterSpacing: {
+        widest: '.06em',
         wide: '.01em',
         narrow: '-.03em',
         narrowest: '-.05em',
